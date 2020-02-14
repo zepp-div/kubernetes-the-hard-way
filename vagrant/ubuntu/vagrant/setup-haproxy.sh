f8ad6671c6b0a7ba40fb30e145a994e73aeab3f4
Дергоусов Илья Витальевич <dergousov_iv@srvhub.ru>
2020-02-14 18:32:45 +0300

    Поднимаем кластер Kubernetes с помощью kubeadm + haproxy

--- vagrant/ubuntu/vagrant/setup-haproxy.sh
+++ vagrant/ubuntu/vagrant/setup-haproxy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Update /etc/haproxy/haproxy.cfg about other hosts
-cat <<EOF | sudo tee /etc/haproxy/haproxy.cfg 
+cat <<EOF | tee /etc/haproxy/haproxy.cfg
 frontend kubernetes
     bind 192.168.5.30:6443
     option tcplog
@@ -13,7 +13,6 @@ backend kubernetes-master-nodes
     balance roundrobin
     option tcp-check
     server master-1 192.168.5.11:6443 check fall 3 rise 2
-    server master-2 192.168.5.12:6443 check fall 3 rise 2
 EOF
 
 systemctl restart haproxy
