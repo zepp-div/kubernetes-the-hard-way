f25ed4aa24fff5f0e057b574edff158aa40bc9a3
zepp-div <zepp4in@gmail.com>
2020-02-16 00:11:47 +0300

    1

---
+++ vagrant/ubuntu/vagrant/setup-routing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat <<EOF | tee /etc/netplan/60-routing.yaml
+---
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $1:        
+      gateway4: $2
+
+EOF
+
+netplan apply
